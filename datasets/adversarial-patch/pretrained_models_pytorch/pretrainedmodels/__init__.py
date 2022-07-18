5e698694337b63a9f6f36bc792757c01e4b9f1a6
luulinh90s <giang@gpu2.cse.eng.auburn.edu>
2022-07-18 11:53:12 -0500

    update

---
+++ datasets/adversarial-patch/pretrained_models_pytorch/pretrainedmodels/__init__.py
@@ -0,0 +1,7 @@
+from .bninception import *
+from .fbresnet import *
+from .inceptionresnetv2 import *
+from .inceptionv4 import *
+from .nasnet import *
+from .resnext import *
+from .torchvision import *

63674a815f338614fb549e842467f75a867d2b84
taesiri <mtaesiri@gmail.com>
2022-07-27 19:46:45 -0500

    add missing class

--- ResNet-50/CUB-iNaturalist/FeatureExtractors.py
+++ ResNet-50/CUB-iNaturalist/FeatureExtractors.py
@@ -397,3 +397,125 @@ class ResNet_classifier(nn.Module):
     def __repr__(self):
         template = "resnet{}_features"
         return template.format(self.num_layers() + 1)
+
+
+class ResNet_AvgPool_classifier(nn.Module):
+    """
+    A classifier for Deformable ProtoPNet
+    """
+
+    def __init__(self, block, layers, num_classes=1000, zero_init_residual=False):
+        super(ResNet_AvgPool_classifier, self).__init__()
+
+        self.inplanes = 64
+
+        # the first convolutional layer before the structured sequence of blocks
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        # comes from the first conv and the following max pool
+        self.kernel_sizes = [7, 3]
+        self.strides = [2, 2]
+        self.paddings = [3, 1]
+
+        # the following layers, each layer is a sequence of blocks
+        self.block = block
+        self.layers = layers
+        self.layer1 = self._make_layer(
+            block=block, planes=64, num_blocks=self.layers[0]
+        )
+        self.layer2 = self._make_layer(
+            block=block, planes=128, num_blocks=self.layers[1], stride=2
+        )
+        self.layer3 = self._make_layer(
+            block=block, planes=256, num_blocks=self.layers[2], stride=2
+        )
+        self.layer4 = self._make_layer(
+            block=block, planes=512, num_blocks=self.layers[3], stride=2
+        )
+
+        self.avgpool = nn.AvgPool2d(kernel_size=(7, 7), stride=1)
+        self.classifier = nn.Linear(2048, 200)
+
+        # initialize the parameters
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _make_layer(self, block, planes, num_blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        # only the first block has downsample that is possibly not None
+        layers.append(block(self.inplanes, planes, stride, downsample))
+
+        self.inplanes = planes * block.expansion
+        for _ in range(1, num_blocks):
+            layers.append(block(self.inplanes, planes))
+
+        # keep track of every block's conv size, stride size, and padding size
+        for each_block in layers:
+            (
+                block_kernel_sizes,
+                block_strides,
+                block_paddings,
+            ) = each_block.block_conv_info()
+            self.kernel_sizes.extend(block_kernel_sizes)
+            self.strides.extend(block_strides)
+            self.paddings.extend(block_paddings)
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x).squeeze()
+        x = self.classifier(x)
+        return x
+
+    def conv_info(self):
+        return self.kernel_sizes, self.strides, self.paddings
+
+    def num_layers(self):
+        """
+        the number of conv layers in the network, not counting the number
+        of bypass layers
+        """
+
+        return (
+            self.block.num_layers * self.layers[0]
+            + self.block.num_layers * self.layers[1]
+            + self.block.num_layers * self.layers[2]
+            + self.block.num_layers * self.layers[3]
+            + 1
+        )
+
+    def __repr__(self):
+        template = "resnet{}_features"
+        return template.format(self.num_layers() + 1)
